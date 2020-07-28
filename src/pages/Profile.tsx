5b38cb0a2901771e6065315bf993580c0bf38027
Yuta Takahashi <takahashi@corepack.co.jp>
2020-07-28 15:18:25 +0900

    add: Show user data in profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom'
+import firebase from '../lib/firebase'
+import {UserData} from "../class/UserData";
 
-const Profile: React.FC= () => {
+const Profile: React.FC = () => {
+  const [user, setUser] = useState({
+    displayName: "",
+    email: ""
+  })
+
+  useEffect(() => {
+    const currentUser = firebase.auth().currentUser
+
+    // Create an instance of UserData
+    const userData = new UserData({
+      uid: currentUser?.uid
+    })
+
+    // Store user data in state
+    userData.getUserData().then(data => setUser(data))
+  }, [])
+
   return (
     <div>
       <p>PROFILE</p>
+      <p>{user.displayName}</p>
+      <p>{user.email}</p>
       <Link to="/">HOME</Link>
     </div>
   );
