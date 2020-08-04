6be5c81fd8d35b06330b11d5b4581c999171fc46
Yuta Takahashi <penityai@gmail.com>
2020-08-05 00:11:28 +0900

    refactor: Corrected comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-// HOC for authentication
+// Composition for Authentication
 import AuthRedirectLogin from "./helpers/AuthRedirectLogin";
 
 // Page Components
