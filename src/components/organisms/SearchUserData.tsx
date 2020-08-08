23fd5ad78b71b57359dd05d4cd02a0da39bd68c2
Yuta Takahashi <penityai@gmail.com>
2020-08-08 22:49:56 +0900

    change: Do not display results if the search word is empty

--- src/components/organisms/SearchUserData.tsx
+++ src/components/organisms/SearchUserData.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import algolia from "../../lib/algolia";
+import {searchClient} from "../../lib/algolia";
 import {InstantSearch} from "react-instantsearch-dom";
 import HitsUserData from "../molecules/HitsUserData";
 import SearchBoxUserData from "../molecules/SearchBoxUserData";
 
 const SearchUserData: React.FC = () => (
-  <InstantSearch indexName="users" searchClient={algolia}>
+  <InstantSearch indexName="users" searchClient={searchClient}>
     <SearchBoxUserData />
     <HitsUserData />
   </InstantSearch>
--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -3,4 +3,23 @@ import algoliasearch from 'algoliasearch'
 const appId = `${process.env.REACT_APP_ALGOLIA_APP_ID}`
 const apiKey = `${process.env.REACT_APP_ALGOLIA_SEARCH_ONLY}`
 
-export default algoliasearch(appId, apiKey);
+export const algoliaClient = algoliasearch(appId, apiKey);
+
+export const searchClient = {
+  // @ts-ignore
+  search(requests) {
+    // @ts-ignore
+    if (requests.every(({params}) => !params.query)) {
+      return Promise.resolve({
+        results: requests.map(() => ({
+          hits: [],
+          nbHits: 0,
+          nbPages: 0,
+          processingTimeMS: 0,
+        })),
+      });
+    }
+
+    return algoliaClient.search(requests);
+  }
+};
