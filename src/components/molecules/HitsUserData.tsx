6a5e8221021300081894428368b97229a2617a00
Yuta Takahashi <penityai@gmail.com>
2020-08-09 21:10:29 +0900

    change: Changed so that my data is not output to user search

--- src/components/molecules/HitsUserData.tsx
+++ src/components/molecules/HitsUserData.tsx
@@ -1,20 +1,37 @@
 import {Hit} from "react-instantsearch-core";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {connectHits} from "react-instantsearch-dom";
+import firebase from "../../lib/firebase";
 
 type Props = {
   hits: Hit[];
 }
 
-const CustomHits: React.FC<Props> = ({hits}) => (
-  <ul>
-    {hits.map((h) => (
-      <li key={h.objectID}>
-        <div>{h.displayName}</div>
-        <div>{h.userId}</div>
-      </li>
-    ))}
-  </ul>
-)
+const CustomHits: React.FC<Props> = ({hits}) => {
+  const [user, setUser] = useState<string | null>(null)
+
+  useEffect(() => {
+    // Get currently logged in user
+    const currentUser = firebase.auth().currentUser
+
+    // Set uid to "setUser"
+    if (currentUser) setUser(currentUser.uid)
+  }, [])
+
+  return user ? (
+    <ul>
+      {hits
+        .filter(h => h.objectID !== user) // Exclude my user data
+        .map((h) => (
+          <li key={h.objectID}>
+            <div>{h.displayName}</div>
+            <div>{h.userId}</div>
+          </li>
+        ))}
+    </ul>
+  ) : null;
+
+
+}
 
 export default connectHits(CustomHits)
