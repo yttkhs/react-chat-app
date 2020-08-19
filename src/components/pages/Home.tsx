bf559d2b83930697f51e39f4484eac295f79bdc0
Yuta Takahashi <penityai@gmail.com>
2020-08-19 19:07:17 +0900

    refactor: Change the URL of the user's profile page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import ButtonLogout from "../atoms/ButtonLogout";
 import SearchUserData from "../organisms/SearchUserData";
-import {useSelector} from "react-redux";
-import { RootState, UserDataProperties } from '../../types';
 
 type Props = {}
 
 const Home: React.FC<Props> = () => {
-  const {userId} = useSelector<RootState, UserDataProperties>(({userData}) => userData)
-
   return (
     <div>
       <p>HOME</p>
       <SearchUserData />
-      <Link to={userId}>PROFILE</Link>
+      <Link to='/profile'>PROFILE</Link>
       <ButtonLogout />
     </div>
   );
