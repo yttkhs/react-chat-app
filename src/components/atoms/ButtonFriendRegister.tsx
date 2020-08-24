2b78e0e72cb4ef9a935ba0aef0fa5d0e0dab853f
Yuta Takahashi <penityai@gmail.com>
2020-08-24 23:13:17 +0900

    change: Changed to add display name and biography when registering as a friend

--- src/components/atoms/ButtonFriendRegister.tsx
+++ src/components/atoms/ButtonFriendRegister.tsx
@@ -15,28 +15,45 @@ const ButtonFriendRegister: React.FC<Props> = ({hits}) => {
   const userData = useSelector<RootState, UserDataProperties>(({userData}) => userData)
   const dispatch = useDispatch()
 
+  // Create friend data object
+  const createFriendDataObject = (
+    uid: string,
+    data: Pick<UserDataProperties, "userId" | "displayName" | "biography">
+  ) => ({
+    friend: {
+      [data.userId]: {
+        roomId: uid,
+        displayName: data.displayName,
+        biography: data.biography
+      }
+    }
+  })
+
   const handleClick = () => {
 
     // Create unique ID
     const uid = firebase.firestore().collection('users').doc().id
 
-    // A friend object that stores a unique ID
-    const friendObject = (user: string) => ({
-      friend: {
-        [user]: {
-          roomId: uid
-        }
-      }
+    const newFriendData = createFriendDataObject(uid, {
+      userId: hits.userId,
+      displayName: hits.displayName,
+      biography: hits.biography
+    })
+
+    const selfFriendData = createFriendDataObject(uid, {
+      userId: userData.userId,
+      displayName: userData.displayName,
+      biography: userData.biography
     })
 
     // Store data in Redux
-    dispatch(userDataAction.add(friendObject(hits.userId)))
+    dispatch(userDataAction.add(newFriendData))
 
     // Store your user data in the friend list data
-    new UserData(userData.userId).updateUserData(friendObject(hits.userId))
+    new UserData(userData.userId).updateUserData(newFriendData)
 
     // Add friend list data to friend's user data
-    new UserData(hits.userId).updateUserData(friendObject(userData.userId))
+    new UserData(hits.userId).updateUserData(selfFriendData)
 
     // Create a chat room
     new ChatData(uid).createChatData()
