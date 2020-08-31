26bc60162427f3f53d434e204a3010a253e4ae83
Yuta Takahashi <penityai@gmail.com>
2020-08-31 17:12:25 +0900

    change: Changed algolia settings to show all users

--- src/components/molecules/SidebarUsersPanel.tsx
+++ src/components/molecules/SidebarUsersPanel.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import SidebarUsersSearchForm from './SidebarUsersSearchForm';
 import SidebarUsersList from "./SidebarUsersList";
 import { SidebarPanelComponent } from '../../types';
-import {searchClient} from "../../lib/algolia";
+import {algoliaClient} from "../../lib/algolia";
 import {InstantSearch} from "react-instantsearch-dom";
 
 const SidebarUsersPanel: React.FC<SidebarPanelComponent> = ({value, index}) => {
   return value === index
     ? (
-      <InstantSearch indexName="users" searchClient={searchClient}>
+      <InstantSearch indexName="users" searchClient={algoliaClient}>
         <SidebarUsersSearchForm />
         <SidebarUsersList />
       </InstantSearch>
