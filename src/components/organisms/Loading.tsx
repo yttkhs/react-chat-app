7e11dc74956efbb8d78b434d4854b513ed8150e9
Yuta Takahashi <penityai@gmail.com>
2020-08-31 17:23:18 +0900

    change: Style adjustment

--- src/components/organisms/Loading.tsx
+++ src/components/organisms/Loading.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {CircularProgress} from '@material-ui/core';
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: theme.spacing(2)
+    }
+  })
+)
 
 const Loading: React.FC = () => {
+  const classes = useStyles()
+
   return (
-    <p>Loading...</p>
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
   );
 };
 
