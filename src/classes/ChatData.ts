02fecf0051795c97f9511ef0d76e5b1c3ecbd620
Yuta Takahashi <penityai@gmail.com>
2020-08-31 17:07:11 +0900

    change: Change the data passing of Chat Data

--- src/classes/ChatData.ts
+++ src/classes/ChatData.ts
@@ -1,28 +1,38 @@
 import firebase from '../lib/firebase'
+import {ChatDataAuthorsProperties, ChatDataLog, ChatDataProperties} from '../types'
 
 export class ChatData {
   constructor(readonly roomId: string) {
   }
 
-  createChatData() {
-    const initData = {
-      initial: {
-        timestamp: Date.now(),
-        text: "Start a chat.",
-        author: "none"
-      }
+  createChatData(firstAuthor: string, secondAuthor: string) {
+    const authorsData: ChatDataAuthorsProperties = {
+      firstAuthor: firstAuthor,
+      secondAuthor: secondAuthor
     }
 
     firebase.database()
-      .ref('chat/' + this.roomId)
-      .set(initData)
+      .ref('chat/' + this.roomId + '/authors/')
+      .set(authorsData)
       .catch(e => console.log(e))
+  }
+
+  fetchLastChatLog() {
+    return firebase.database()
+      .ref('chat/' + this.roomId)
+      .once('value')
+      .then((snapshot) => {
+        const value: ChatDataProperties | undefined = snapshot.val()
+        return value?.log
+          ? Object.entries(value.log).map(value => value[1]).slice(-1)[0]
+          : undefined;
+      })
   }
 
   addChatLog(text: string, author: string) {
     const randomKey = firebase.database().ref().child('chat').push().key!;
 
-    const updateData = {
+    const logData: ChatDataLog = {
       [randomKey]: {
         timestamp: Date.now(),
         text: text,
@@ -31,8 +41,8 @@ export class ChatData {
     }
 
     firebase.database()
-      .ref('chat/' + this.roomId)
-      .update(updateData)
+      .ref('chat/' + this.roomId + '/log/')
+      .update(logData)
       .catch(e => console.log(e))
   }
 }
